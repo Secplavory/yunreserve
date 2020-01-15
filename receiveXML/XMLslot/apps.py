6b3293e5f39a67361e9959979eac1e6cdccd7740
Flavored <Secplavory@users.noreply.github.com>
2020-01-15 14:44:28 +0800

    將網站專案分開放置

--- receiveXML/XMLslot/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class XmlslotConfig(AppConfig):
-    name = 'XMLslot'
--- receiveXML/XMLslot/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-XMLslot_Patterns = [
-    url('', views.receivePost),
-]
